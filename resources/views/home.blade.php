cf4a17de8a11649d008b9c994b343220b217ff4d
Rizanoor <rizanoorfauzan123@gmail.com>
2021-11-09 08:11:48 +0700

    Belajar struktur folder,Routes & view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Blog | Home</title>
+</head>
+<body>
+
+  <h1>Halaman Home</h1>
+  <h3>Rizanoorfuazan</h3>
+  <p>Rizanoorfuazan@gmail.com</p>
+  <img src="img/icons-testimonial-1.png" alt="">
+
+  <script src="js/script.js"></script>
+</body>
+</html>
