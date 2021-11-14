d3a0ca689dbc03e63c2b4db402c5a694e9b17ef7
Rizanoor <rizanoorfauzan123@gmail.com>
2021-11-15 06:09:26 +0700

    belajar post dan model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,14 +2,13 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman Post</h1>
+<h1>Halaman Blog Posts</h1>
 <br>
   @foreach ($posts as $post)
     <article class="mb-4">
       <h3>
-        <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }} </a></h3>
-      <h5>by: {{ $post["author"] }}</h5>
-      <p>{{ $post["body"] }}</p>
+        <a href="/posts/{{ $post->slug }}">{{ $post->title }} </a></h3>
+      <p>{{ $post->excerpt }}</p>
     </article>
   @endforeach
 
