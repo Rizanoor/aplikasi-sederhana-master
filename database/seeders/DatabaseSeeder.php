a23e3383f8fc340a29a1890a3e6b0b1c7a5c4e16
Rizanoor <rizanoorfauzan123@gmail.com>
2021-11-17 10:50:32 +0700

    Belajar dtabase seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Rizanoorfauzan',
+            'email' => 'riza@gmail.com',
+            'password' => bcrypt('1212')
+        ]);
+
+        User::create([
+            'name' => 'Alqis',
+            'email' => 'alqis@gmail.com',
+            'password' => bcrypt('1212')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing',
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, maiores',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ducimus quod quaerat assumenda nisi beatae culpa placeat sunt aspernatur veritatis soluta doloribus odit commodi nulla quo quam quos perspiciatis accusantium minus? Tempora voluptatum error facilis, similique sed illum optio!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, maiores',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ducimus quod quaerat assumenda nisi beatae culpa placeat sunt aspernatur veritatis soluta doloribus odit commodi nulla quo quam quos perspiciatis accusantium minus? Tempora voluptatum error facilis, similique sed illum optio!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, maiores',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ducimus quod quaerat assumenda nisi beatae culpa placeat sunt aspernatur veritatis soluta doloribus odit commodi nulla quo quam quos perspiciatis accusantium minus? Tempora voluptatum error facilis, similique sed illum optio!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, maiores',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ducimus quod quaerat assumenda nisi beatae culpa placeat sunt aspernatur veritatis soluta doloribus odit commodi nulla quo quam quos perspiciatis accusantium minus? Tempora voluptatum error facilis, similique sed illum optio!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,10 +5,19 @@
 <h1>Halaman Blog Posts</h1>
 <br>
   @foreach ($posts as $post)
-    <article class="mb-4">
-      <h3>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }} </a></h3>
+    <article class="mb-5 border-bottom pb-4">
+      <h3><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }} </a></h3>
+      
+      <p>By. 
+        <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> 
+        in 
+        <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+          {{ $post->category->name }}</a>
+      </p>
+
       <p>{{ $post->excerpt }}</p>
+
+      <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
     </article>
   @endforeach
 
