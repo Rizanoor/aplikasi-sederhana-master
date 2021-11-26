6ae9387e5401bd9d218b2b52784249920912c3c9
Rizanoor <rizanoorfauzan123@gmail.com>
2021-11-26 08:19:48 +0700

    membuat login dan register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,11 +10,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <style>
+    {{-- Bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    {{-- My style --}}
+    <link rel="stylesheet" href="/css/style.css">
+    {{-- <style>
       body{
         font-family: 'Poppins', sans-serif;
       }
-    </style>
+
+    </style> --}}
     <title>Blog | {{ $title }}</title>
   </head>
   <body>
@@ -25,7 +30,17 @@
       @yield('container')
     </div>
 
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2"> 2020 Copyright Blogs. All Right Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
 
+      {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 use App\Models\Category;
-use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,18 +51,5 @@ Route::get('/categories', function () {
     ]);
 });
 
-// Route::get('/categories/{category:slug}', function (Category $category) {
-//     return view('posts', [
-//         'title' => " Post By Category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category', 'author')
-//     ]);
-// });
-
-// Route::get('/authors/{author:username}', function (User $author) {
-//     return view('posts', [
-//         'title' => "Post By Author : $author->name",
-//         'active' => 'posts',
-//         'posts' => $author->posts->load('category', 'author'),
-//     ]);
-// });
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
