9388f791920b3336662c288347849501aee276ec
Rizanoor <rizanoorfauzan123@gmail.com>
2021-11-30 07:50:52 +0700

    membuat dashboard UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,23 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+  <div class="row my-3">
+    <div class="col-lg-8">
+      <h2 class="mb-3"> {{ $post->title }}</h2>
+        <a href="/dashboard/posts" class="btn btn-success">Back</a>
+        <a href="" class="btn btn-warning">Edit</a>
+        <a href="" class="btn btn-danger">Delete</a>
+    
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3" style="border-radius: 20px">
+
+        <article class="my-3 fs-5">
+          {!! $post->body !!}
+        </article>
+    
+        <br>
+        <a href="/posts" class="btn btn-primary mt-3">Back to Blog</a>
+    </div>
+  </div>
+</div>
+@endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -33,7 +33,7 @@
                       <form action="/logout" method="post">
                         @csrf
                         <button type="submit" class="dropdown-item">
-                          <i class="bi bi-box-arrow-in-right"></i> Logout</a>
+                          <i class="bi bi-box-arrow-in-right"></i> Sign out</a>
                         </button>
                       </form>
                     </li>
